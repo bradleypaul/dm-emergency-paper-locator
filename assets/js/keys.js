909cbb1fa9a8c5f86f46fb17dc477a47d67f46b9
paul <bradleypauld@gmail.com>
2020-04-29 19:25:07 -0500

    add keys and hosts

---
+++ assets/js/keys.js
@@ -0,0 +1,3 @@
+const axsessoKey = `LIf3v3u97Wmshek4PIKJGfwmDRHHp1e33VnjsnxVU7ZUW0fu5W`;
+const walmartHost = `axesso-walmart-data-service.p.rapidapi.com`;
+const amazonHost = `axesso-axesso-amazon-data-service-v1.p.rapidapi.com`;
