45ac0703d71be72259eecf26af3b48d288ac1828
kIvy1189 <kevindivy@gmail.com>
2020-05-02 09:38:49 -0500

    added ability to search for selected product. Updated functions to use searchterm and load product results into searchResults array as objects

--- assets/js/keys.js
+++ assets/js/keys.js
@@ -1,59 +1,79 @@
 const axsessoKey = `LIf3v3u97Wmshek4PIKJGfwmDRHHp1e33VnjsnxVU7ZUW0fu5W`;
 const walmartHost = `axesso-walmart-data-service.p.rapidapi.com`;
 const amazonHost = `axesso-axesso-amazon-data-service-v1.p.rapidapi.com`;
-const searchTerm = 'Playstation'
-fetch(`https://axesso-axesso-amazon-data-service-v1.p.rapidapi.com/amz/amazon-search-by-keyword-asin?sortBy=relevanceblender&domainCode=com&keyword=${searchTerm}&page=1`, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": amazonHost,
-		"x-rapidapi-key": axsessoKey
-	}
-})
-.then(response => response.json())
-.then(result => console.log(result))
-.catch(err => {
-	console.log(err);
-});
+const searchButtonEl = document.querySelector('#searchProduct');
+var searchTerm = ''
+var searchResults = [];
 
+//get the product url for the first 3 results from Amazon
+function getAmazonUrl() {
+	fetch(`https://axesso-axesso-amazon-data-service-v1.p.rapidapi.com/amz/amazon-search-by-keyword-asin?sortBy=relevanceblender&domainCode=com&keyword=${searchTerm}&page=1`, {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": amazonHost,
+			"x-rapidapi-key": axsessoKey
+		}
+	})
+	.then(response => {
+		if (response.ok) {
+			response.json().then(function(data){
+				//loop through the top three results to get asin
+				for(i =0; i < 3; i++) {
+					var asin = data.searchProductDetails[i].asin;
+					//load asin to get specific product details
+					getAmazonProduct(asin);
+				}
+			});
+		}
+	});
+};
 
-fetch(`https://axesso-walmart-data-service.p.rapidapi.com/wlm/walmart-search-by-keyword?sortBy=best_match&page=1&keyword=${searchTerm}&type=text`, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": walmartHost,
-		"x-rapidapi-key": axsessoKey
-	}
-})
-.then(response => response.json())
-.then(result => console.log(result))
-.catch(err => {
-	console.log(err);
-});
+// fetch(`https://axesso-walmart-data-service.p.rapidapi.com/wlm/walmart-search-by-keyword?sortBy=best_match&page=1&keyword=${searchTerm}&type=text`, {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": walmartHost,
+// 		"x-rapidapi-key": axsessoKey
+// 	}
+// })
+// .then(response => response.json())
+// .then(result => console.log(result))
+// .catch(err => {
+// 	console.log(err);
+// });
 
-fetch("https://axesso-walmart-data-service.p.rapidapi.com/wlm/walmart-lookup-product?url=https://www.walmart.com/ip/Sony-PlayStation-4-Slim-500GB-Gaming-Console-Black-CUH-2115A/536117094", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "axesso-walmart-data-service.p.rapidapi.com",
-		"x-rapidapi-key": "LIf3v3u97Wmshek4PIKJGfwmDRHHp1e33VnjsnxVU7ZUW0fu5W"
-	}
-})
-.then(response => response.json())
-.then(result => console.log(makeWalmartProduct(result)))
-.catch(err => {
-	console.log(err);
-});
+// fetch("https://axesso-walmart-data-service.p.rapidapi.com/wlm/walmart-lookup-product?url=https://www.walmart.com/ip/Sony-PlayStation-4-Slim-500GB-Gaming-Console-Black-CUH-2115A/536117094", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "axesso-walmart-data-service.p.rapidapi.com",
+// 		"x-rapidapi-key": "LIf3v3u97Wmshek4PIKJGfwmDRHHp1e33VnjsnxVU7ZUW0fu5W"
+// 	}
+// })
+// .then(response => response.json())
+// .then(result => console.log(makeWalmartProduct(result)))
+// .catch(err => {
+// 	console.log(err);
+// });
 
-fetch("https://axesso-axesso-amazon-data-service-v1.p.rapidapi.com/amz/amazon-lookup-product?url=https://www.amazon.com/PlayStation-Slim-1TB-Console-Bundle-4/dp/B07YLDNTKB", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "axesso-axesso-amazon-data-service-v1.p.rapidapi.com",
-		"x-rapidapi-key": "LIf3v3u97Wmshek4PIKJGfwmDRHHp1e33VnjsnxVU7ZUW0fu5W"
-	}
-})
-.then(response => response.json())
-.then(result => console.log(makeAmazonProduct(result)))
-.catch(err => {
-	console.log(err);
-});
+function getAmazonProduct(asin) {
+	fetch("https://axesso-axesso-amazon-data-service-v1.p.rapidapi.com/amz/amazon-lookup-product?url=https://www.amazon.com/dp/" + asin, {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "axesso-axesso-amazon-data-service-v1.p.rapidapi.com",
+			"x-rapidapi-key": "LIf3v3u97Wmshek4PIKJGfwmDRHHp1e33VnjsnxVU7ZUW0fu5W"
+		}
+	})
+	.then(response => {
+		//verify a result was received
+		if (response.ok) {
+			response.json().then(function(data){
+				var productDetails = makeAmazonProduct(data);
+				searchResults.push(productDetails);
+				console.log('Done');
+			})
+		}
+	});
+};
+
 
 function makeAmazonProduct(product) {
 	return {
@@ -77,3 +97,16 @@ function makeWalmartProduct(product) {
 function isAvailable(availabilityString) {
 	return availabilityString.toLowerCase().includes('available')
 }
+
+function setSearchTerm(event) {
+	//prevent page reload
+	event.preventDefault();
+
+	//get the current selected value
+	searchTerm = document.querySelector('#productSelection').value;
+	
+	//call function to fetch product details
+	getAmazonUrl();
+};
+
+searchButtonEl.addEventListener('click', setSearchTerm);
